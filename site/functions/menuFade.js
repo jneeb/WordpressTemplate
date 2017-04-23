3f2583c8e137c2b75c2e4a5ae8f004aa84b29181
jneeb <neebjustin@gmail.com>
2017-04-22 23:43:13 -0400

    laying ground work for menu section fade in and out

--- site/functions/menuFade.js
+++ site/functions/menuFade.js
@@ -1,13 +1,20 @@
-$(document).ready(function () {
-	$(".menuSelect").click(function () {
-		$(".menuSelect").removeClass("active");
-		$(this).addClass("active");
-	});
-});
+// $(document).ready(function () {
+// 	$(".menuSelect").click(function () {
+// 		$(".menuSelect").removeClass("active");
+// 		$(this).addClass("active");
+// 	});
+// });
+//
+// $(document).ready(function () {
+// 	$(".lunch").click(function () {
+// 		$(".menuFade").fadeOut("slow");
+// 		$(".lunchMenu").addClass("active");
+// 	});
+// });
 
 $(document).ready(function () {
-	$(".lunch").click(function () {
-		$(".menuFade").fadeOut("slow");
-		$(".lunchMenu").addClass("active");
+	$("#lunch").click(function () {
+		$("#breakfastMenu").fadeOut(800);
+		$("#lunchMenu").fadeIn(800);
 	});
 });
